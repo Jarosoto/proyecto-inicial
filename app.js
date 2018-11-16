8a23463a2bde953a2c8066ec39b8c47e8bf8900a
Jarosoto <javier.rguez.soto@gmail.com>
2018-11-16 12:57:56 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Un archivo simple de TypeScript
+function saludar(nombre) {
+  console.log("sadasd");
+    console.log("Hola: " + nombre);
+}
+var wolverine = {
+    nombre: "Logan"
+};
+saludar(wolverine.nombre);
